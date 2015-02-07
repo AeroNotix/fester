8204278840b7b9692cb8b1741570028aaff7555b
Aaron France <aaron.l.france@gmail.com>
2015-02-07 14:03:12 +0100

    Naive rollups working

--- src/fester/bolts.clj
+++ src/fester/bolts.clj
@@ -10,14 +10,13 @@
   (cql/insert conn table {:time ts :key key :name name :value value}))
 
 (defn store-average-in-cass [conn table values]
-  (let [total (reduce #(+ %1 (:foo %2)) 0 values)
+  (let [total (reduce #(+ %1 (%2 3)) 0 values)
         avg   (/ total (float (count values)))
         [ts key name] (first values)]
-    (println values)
     (write-to-cassandra conn table ts key name avg)))
 
-(defn period-lasts? [duration stored]
-  (> (- (:ts (last stored)) (:ts (first stored))) duration))
+(defn period-lasts? [[ts1 & _] [ts2 & _] duration]
+  (> (- ts2 ts1) duration))
 
 (defbolt fester-raw-metric-bolt ["ts" "key" "name" "value"] {:prepare true}
   [conf _ collector]
@@ -28,7 +27,7 @@
         (write-to-cassandra conn "raw" ts key name value)
         (ack! collector tuple)))))
 
-(defbolt fester-rollup-metric-bolt ["ts" "key" "name" "value"]
+(defbolt fester-rollup-metric-bolt ["ts" "dt" "key" "name" "value"]
   {:prepare true
    :params [period table]}
   [conf _ collector]
@@ -41,9 +40,9 @@
           (if (not stored)
             (.put nbhm [key name] [[ts key name value]])
             (let [next (conj stored [ts key name value])]
-              (if (period-lasts? 10 next)
+              (if (period-lasts? (first next) (last next) 10)
                 (do
                   (store-average-in-cass conn table next)
                   (.put nbhm [key name] []))
                 (.put nbhm [key name] (conj stored [ts key name value])))))
-          (println stored))))))
+          (emit-bolt! collector [ts period key name value]))))))
